be436a026877c39e366f11e8a82519b0db06ca9c
OnlyFiksa <taufikdarmawan2005@gmail.com>
2025-12-22 13:42:30 +0700

    pushh

--- backend/src/main/java/com/quatre/backend/controller/AdminController.java
+++ backend/src/main/java/com/quatre/backend/controller/AdminController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder; // Import Penting!
+import org.springframework.security.crypto.password.PasswordEncoder; 
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
